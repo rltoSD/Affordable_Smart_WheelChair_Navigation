03930905ac78445f40d629d95435df94e4a39515
Richard To <50217652+rltoSD@users.noreply.github.com>
2019-07-01 16:05:44 -0700

    ToF Sensor Information Subscription & Output

---
+++ Sensor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float64MultiArray
+
+def callback(data):
+	label=data.layout.dim[0].label
+	size=data.layout.dim[0].size
+	stride=data.layout.dim[0].stride
+def main():
+	rospy.init_node('TFS_Information')
+	rospy.subscriber("/Float64MultiArray", Float64MultiArray, callback)
+	rospy.spin()
+if __name__== '__main__':
+	main()
+	
