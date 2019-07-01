9d07dd77bb9b325a1d003b380d18a9d4dc43727a
Aramis69 <38967844+Aramis69@users.noreply.github.com>
2019-07-01 13:10:04 -0700

    Update locationInput.py

--- locationInput.py
+++ locationInput.py
@@ -7,24 +7,17 @@ from nav_msgs.msg import Odometry
 from move_topic.msg import float64
 
 def callback(data):
-    #try to get our dictionary with locations and coordinates
-    try:
-        #load the dictionary or create
-        locationMap = np.load("dictionary.npy")
-    #handle if there is no saved dictionary
-    except FileNotFoundError:
-        #make expandable hashtable
-        new_dict = dict()
-        np.save("dictionary.npy",new_dict)
-        locationMap = np.load("dictionary.npy")
-
+    
+    #load the dictionary assuming this file is already created
+    locationMap = np.load("dictionary.npy").item()
+    
     #seperate string sections to get type of command
     arr = data.data.split(" ")
 
     #if the string starts with go
-    if(arr[0] == 'go')
+    if arr[0] == 'go':
         #check that the location is marked on map
-        if arr[2] in locationMap
+        if arr[2] in locationMap:
             #publish location to be read by SLAM
             pub1 = rospy.Publisher('MoveTopic',float64,queue_size = 1)
             r = rospy.Rate(1)
@@ -38,17 +31,17 @@ def callback(data):
             rospy.loginfo("Location not found : %s",arr[2])
 
     #if command wants to set new location
-    else if(arr[0] == 'mark')
+    else if arr[0] == 'mark':
         #check that its not already marked
-        if arr[2] in locationMap
-            rospy.loginfo("Locatio %s already marked",arr[2])
+        if arr[2] in locationMap:
+            rospy.loginfo("Location %s already marked",arr[2])
         else:
             arrOdometry = [data.pose.pose.position.x,data.pose.pose.position.y,data.pose.pose.position.z]
             locationMap[arr[2]] = arrOdometry
 
     #if command is to delete a saved location
-    else if(arr[0] == 'delete')
-        if arr[2] in locationMap
+    else if arr[0] == 'delete':
+        if arr[2] in locationMap:
             del locationMap[arr2]
         else:
             rospy.loginfo("Location not found : %s",arr[2])
