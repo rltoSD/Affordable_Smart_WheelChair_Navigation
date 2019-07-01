6d6c2f187222af23af4698fda91629a33402e312
Aramis69 <38967844+Aramis69@users.noreply.github.com>
2019-07-01 13:18:00 -0700

    Update createFileOnce.py

--- Alexa/createFileOnce.py
+++ Alexa/createFileOnce.py
@@ -1,1 +1,10 @@
+#only needs to be ran once using "python ./createFileOnce.py"
+#creates a empty dictionary file which we will load in loadDictionary
+import numpy as np
 
+def main():
+  new_dict = dict()
+  np.save("Dictionary.npy", new_dict)
+    
+if __name__ == '__main__':
+  main()
