5d35c3c936fac3ccda900d708ff4eda0c56bb753
Richard To <50217652+rltoSD@users.noreply.github.com>
2019-07-03 14:57:38 -0700

    Changed Callback to access data.data array

--- Sensor.py
+++ Sensor.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python
 import rospy
-#import the message file so we can store it into the Float
 from std_msgs.msg import Float64MultiArray
 
 def callback(data):
-	#stores data into variables
-	label=data.layout.dim[0].label
-	size=data.layout.dim[0].size
-	stride=data.layout.dim[0].stride
-	#prints it out to terminal
-	rospy.loginfo('label: {}, size: {}, stride: {}'.format(label,size,stride))
+        sensor_one=data.data[0]
+        sensor_two=data.data[1]
+        sensor_three=data.data[2]
+        sensor_four=data.data[3]
+        sensor_five=data.data[4]
+        sensor_six=data.data[5]
+        sensor_seven=data.data[6]
+        sensor_eight=data.data[7]
+        sensor_nine=data.data[8]
+        sensor_ten=data.data[9]
+        sensor_eleven=data.data[10]
+        sensor_twelve=data.data[11]
+        rospy.loginfo('sensor_1: {}'.format(sensor_one))
+        rospy.loginfo('sensor_2: {}'.format(sensor_two))
+        rospy.loginfo('sensor_3: {}'.format(sensor_three))
+        rospy.loginfo('sensor_4: {}'.format(sensor_four)
+        rospy.loginfo('sensor_5: {}'.format(sensor_five))
+        rospy.loginfo('sensor_6: {}'.format(sensor_six))
+        rospy.loginfo('sensor_7: {}'.format(sensor_seven))
+        rospy.loginfo('sensor_8: {}'.format(sensor_eight))
+        rospy.loginfo('sensor_9: {}'.format(sensor_nine))
+        rospy.loginfo('sensor_10: {}'.format(sensor_ten))
+        rospy.loginfo('sensor_11: {}'.format(sensor_eleven))
+        rospy.loginfo('sensor_12: {}'.format(sensor_twelve))
 def main():
-	#creates a new node
-	rospy.init_node('TFS_Information')
-	#subscribes to the topic "tof_sensor" so we can receive information
-	rospy.subscriber("/tof_sensor", Float64MultiArray, callback)
-	rospy.spin()
+        rospy.init_node('TFS_Information')
+        rospy.subscriber("/tof_sensor", Float64MultiArray, callback)
+        rospy.spin()
 if __name__== '__main__':
-	main()
-	
+        main()
