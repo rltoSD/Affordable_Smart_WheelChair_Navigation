91ec7c208905f64cccfa96df9187f13f579d7b75
Richard To <50217652+rltoSD@users.noreply.github.com>
2019-07-03 15:03:23 -0700

    Node which uses custom topic to read Encoder data

---
+++ Subscriber/Encoder_custom.py
@@ -0,0 +1,17 @@
+!/usr/bin/env python
+import rospy
+from encoder_msg.msg import Encoder
+
+def callback(data):
+	#calls and stores encoder left and right 
+	Encoder_left=data.Encoder.left
+	Encoder_right=data.Encoder.right
+        rospy.loginfo('Encoder_left: {}, Encoder_right: {}'.format(Encoder_left,Encoder_right)
+def main():
+	#create a new node
+        rospy.init_node('Encoder_Information')
+        #subscribe to topic
+        rospy.Subscriber("/Encoder", Encoder, callback)
+        rospy.spin()
+if __name__== '__main__':
+	main()
