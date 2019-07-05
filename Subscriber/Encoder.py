9eb115b6564c8e977b6152b28d0ffadec3915c77
Evan Serrano <evanjserrano@gmail.com>
2019-07-05 09:11:38 -0700

    Update Encoder.py

--- Subscriber/Encoder.py
+++ Subscriber/Encoder.py
@@ -4,10 +4,10 @@ from std_msgs.msg import Float64
 
 def callback(data):
 	data=data.data
-        rospy.loginfo('Encoder_Left: {}'.format(data.data)
+        rospy.loginfo('Encoder_Left: {}'.format(data.data))
 def callback_two(data):
 	data=data.data
-        rospy.loginfo('Encoder_Right: {}'.format(data.data)
+        rospy.loginfo('Encoder_Right: {}'.format(data.data))
 def main():
 	rospy.init_node('Encoder Information')
 	rospy.subscriber("/encoder_left", Float64, callback)
