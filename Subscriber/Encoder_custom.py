7bf1387a66337ed153f9d41b6d2c8256b5c873c1
Evan Serrano <evanjserrano@gmail.com>
2019-07-05 09:12:40 -0700

    Update Encoder_custom.py

--- Subscriber/Encoder_custom.py
+++ Subscriber/Encoder_custom.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python
+#!/usr/bin/env python
 import rospy
 from encoder_msg.msg import Encoder
 
@@ -6,7 +6,7 @@ def callback(data):
 	#calls and stores encoder left and right 
 	Encoder_left=data.Encoder.left
 	Encoder_right=data.Encoder.right
-        rospy.loginfo('Encoder_left: {}, Encoder_right: {}'.format(Encoder_left,Encoder_right)
+        rospy.loginfo('Encoder_left: {}, Encoder_right: {}'.format(Encoder_left,Encoder_right))
 def main():
 	#create a new node
         rospy.init_node('Encoder_Information')
