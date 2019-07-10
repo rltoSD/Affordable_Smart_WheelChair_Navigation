9d0ad775389c1f5984a9b4105b122b9dc2887b50
Richard To <50217652+rltoSD@users.noreply.github.com>
2019-07-10 10:12:44 -0700

    added comments

--- Google_Speech/gspeech_printer.py
+++ Google_Speech/gspeech_printer.py
@@ -6,11 +6,15 @@ from google.cloud import speech
 from std_msgs.msg import String, Header
 
 def callback(data):
+        #grabs the transcript and confidence and stores them for later use
         transcript=input.transcript
         confidence=input.confidence
+        #prints out the information for user to see
         rospy.loginfo('transcript: {}, confidence{}'.format(transcript,confidence)
 def main():
+        #create a new node
         rospy.init_node('GSPEECH_INPUT_INFORMATION')
+        #grabs the information for transcript and confidence
         rospy.Subscriber("/input", input, callback)
         rospy.spin()
 if __name__=='__main__':
