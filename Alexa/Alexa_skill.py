26a75d4319a666e83eb8756b18ffa30cd3820817
Richard To <50217652+rltoSD@users.noreply.github.com>
2019-07-11 15:08:58 -0700

    Add files via upload

---
+++ Alexa/Alexa_skill.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_ask import Ask, statement, question, session
+from std_msgs.msg import int64
+import json
+import requests
+import time
+import unidecode
+import rospy
+
+app = Flask (__name__)
+ask = Ask(app, "/")
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+input = 0.0
+@ask.launch
+def welcome():
+	welcome_msg = render_template('Where do you want to go?')
+	return question(welcome_msg)
+
+@ask.intent("AnswerIntent", convert={'first': int})
+def movement(first):
+	global input
+	input = first
+	rospy.Publisher('chatter', int64, queue_size=1)
+	rospy.init_node('input', anonymous=True)
+	while not rospy.is_shutdown():
+		rospy.loginfo(input)
+		pub.publish(input)
+		rate.sleep()
+	text = "The location is {}".format(input) 			
+	return statement(text)
