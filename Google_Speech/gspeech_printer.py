b95c0ece313b52fe69bc9998da5a5acf838e12d4
Richard To <50217652+rltoSD@users.noreply.github.com>
2019-07-16 11:17:13 -0700

    Updated nodes and subscribers

--- Google_Speech/gspeech_printer.py
+++ Google_Speech/gspeech_printer.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 
 import rospy
-from gspeech_input_msg import input
-from google.cloud import speech
-from std_msgs.msg import String, Header
+from std_msgs.msg import String
 
 def callback(data):
         #grabs the transcript and confidence and stores them for later use
-        transcript=input.transcript
-        confidence=input.confidence
+        transcript=data.data
         #prints out the information for user to see
-        rospy.loginfo('transcript: {}, confidence{}'.format(transcript,confidence)
+        rospy.loginfo('transcript: {}'.format(transcript)
 def main():
         #create a new node
         rospy.init_node('GSPEECH_INPUT_INFORMATION')
         #grabs the information for transcript and confidence
-        rospy.Subscriber("/input", input, callback)
+        rospy.Subscriber("chatter", String, callback)
         rospy.spin()
 if __name__=='__main__':
         main()
